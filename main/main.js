e235ab4e58aa06dd1366d388a01f6310bdd9f146
MatiasCalvi <alejandropreuss30@gmail.com>
2022-10-18 16:55:03 -0300

    details dinamico JS

--- main/main.js
+++ main/main.js
@@ -1,7 +1,12 @@
 let contenedorCard = document.getElementById("contenedor-cards-general");
 let searchInput = document.getElementById("searchInput");
 let contenedorCheck = document.getElementById("checkbox-contain");
+let contenedorDetails = document.getElementById("contenedorDetails");
 let categories = new Set(events.map((element) => element.category).sort());
+let upcoming = events.filter((event) => event.date > currentDate);
+let past = events.filter((event) => event.date < currentDate);
+let applied = {};
+let categoriesFiltradas = [];
 let hrefHome = "./pages/details.html";
 let hrefOtros = "./details.html";
 
@@ -27,7 +32,7 @@ function htmlCard(array, ruta) {
     <div class="card-body d-flex justify-content-around mt-2 pt-4">
       <p class="card-link mt-2">Price $${array.price}</p>
       <a
-        href="${ruta}"
+        href="${ruta}?id=${array._id}"
         class="btn btn-primary"
         style="height: 39.99px"
         >See more</a
@@ -52,13 +57,42 @@ function htmlCheck(array) {
   `;
 }
 
+function htmlDetails(obj) {
+  contenedorDetails.innerHTML = `
+      <div
+        class="card mb-3 shadow-lg p-3 mb-5 bg-body rounded d-flex flex-column justify-content-center"
+        style="width: 100%; height: 35rem contenedor-cards-general"
+      >
+        <div class="row g-0">
+          <div class="col-md-6 p-4">
+            <img
+              src=${obj.image}
+              class="img-fluid rounded-end mt-sm-4 mt-md-0 w-100 h-100"
+              alt="foto-maraton"
+            />
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+            <h6 class="text-center pt-1 pb-1 text-bg-dark fs-5">${obj.category}</h6>
+              <h1 class="card-title text-center fs-3 text-decoration-underline">${obj.name}</h1>
+              <p class="text-center fs-4">${obj.date}</p>
+              <p class="card-text pb-2 fs-5 text-center">
+              ${obj.description}
+              </p>
+              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Place: </span>${obj.place}</p>
+              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Capacity: </span>${obj.capacity}</p>
+              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Assistance: </span>${obj.assistance}</p>
+              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Price:</span> $${obj.price}</p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+}
+
 function imprimir(array, ruta) {
   contenedorCard.innerHTML = "";
   array.forEach((e) => htmlCard(e, ruta));
 }
-
-let applied = {};
-let categoriesFiltradas = [];
 
 function filterFn(fn, value, array, check) {
   applied[fn] = value;
@@ -96,15 +130,10 @@ function filterFn(fn, value, array, check) {
   return array;
 }
 
-let upcoming = events.filter((event) => event.date > currentDate);
-let past = events.filter((event) => event.date < currentDate);
-
-categories.forEach((events) => htmlCheck(events));
-
 switch (document.title) {
   case "Upcoming Events-Amazing Events":
     imprimir(upcoming, hrefOtros);
-
+    categories.forEach((events) => htmlCheck(events));
     searchInput.addEventListener("input", (event) => {
       let escribir = filterFn(
         "matchWithSearchBar",
@@ -127,13 +156,9 @@ switch (document.title) {
 
   case "Past Events-Amazing Events":
     imprimir(past, hrefOtros);
-
+    categories.forEach((events) => htmlCheck(events));
     searchInput.addEventListener("input", (event) => {
-      let escribir = filterFn(
-        "matchWithSearchBar",
-        event.target.value,
-        past
-      );
+      let escribir = filterFn("matchWithSearchBar", event.target.value, past);
       imprimir(escribir, hrefOtros);
     });
 
@@ -148,15 +173,18 @@ switch (document.title) {
     });
     break;
 
+  case "Details Event Amazing Events":
+    let id = location.search.slice(4);
+    let details = events.filter((element) => element._id === Number(id));
+    details = details[0];
+    console.log(details);
+    htmlDetails(details);
+    break;
   default:
     imprimir(events, hrefHome);
-
+    categories.forEach((events) => htmlCheck(events));
     searchInput.addEventListener("input", (event) => {
-      let escribir = filterFn(
-        "matchWithSearchBar",
-        event.target.value,
-        events
-      );
+      let escribir = filterFn("matchWithSearchBar", event.target.value, events);
       imprimir(escribir, hrefHome);
     });
 
