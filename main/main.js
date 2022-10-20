69683de1a21cde7030ffc56594a608b915190eb6
MatiasCalvi <alejandropreuss30@gmail.com>
2022-10-20 20:31:05 -0300

    data eliminado y inclusion de api y propiedades Async

--- main/main.js
+++ main/main.js
@@ -2,9 +2,6 @@ let contenedorCard = document.getElementById("contenedor-cards-general");
 let searchInput = document.getElementById("searchInput");
 let contenedorCheck = document.getElementById("checkbox-contain");
 let contenedorDetails = document.getElementById("contenedorDetails");
-let categories = new Set(events.map((element) => element.category).sort());
-let upcoming = events.filter((event) => event.date > currentDate);
-let past = events.filter((event) => event.date < currentDate);
 let applied = {};
 let categoriesFiltradas = [];
 let hrefHome = "./pages/details.html";
@@ -14,25 +11,25 @@ function htmlCard(array, ruta) {
   contenedorCard.innerHTML += `
     <div
     class="card m-3 shadow-lg p-3 mb-5 bg-body rounded"
-    style="width: 18rem ; height:30rem" 
+    style="width: 18rem ; height:35rem" 
   >
     <img
       src="${array.image}"
-      class="card-img-top "
-      height="171"
-      alt="foto-disfraces"
+      class="card-img-top pb-2"
+      height="180"
+      alt="${array.name}"
     />
-    <div class="card-body marginCardBody">
+    <div class="card-body marginCardBody"style="height:20rem">
       <h6 class="card-title text-center pt-1 pb-2 rounded-pill text-bg-dark ">${array.category}</h6>
       <h5 class="card-title pt-2 text-center text-decoration-underline">${array.name}</h5>
-      <p class="card-text">
+      <p class="card-text text-center pt-3">
         ${array.description}
       </p>
     </div> 
-    <div class="card-body d-flex justify-content-around mt-2 pt-4">
+    <div class="card-body d-flex justify-content-around mt-2 pt-5">
       <p class="card-link mt-2">Price $${array.price}</p>
       <a
-        href="${ruta}?id=${array._id}"
+        href="${ruta}?id=${array.id}"
         class="btn btn-primary"
         style="height: 39.99px"
         >See more</a
@@ -57,7 +54,7 @@ function htmlCheck(array) {
   `;
 }
 
-function htmlDetailsUpcoming(obj) {
+function htmlDetails(obj,frase,propiedad) {
   contenedorDetails.innerHTML = `
       <div
         class="card  shadow-lg pb-2 bg-body rounded d-flex flex-column justify-content-center"
@@ -81,43 +78,10 @@ function htmlDetailsUpcoming(obj) {
               </p>
               <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Place: </span>${obj.place}</p>
               <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Capacity: </span>${obj.capacity}</p>
-              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Expectative: </span>${obj.assistance}</p>
+              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">${frase}: </span>${obj[propiedad]}</p>
               <p class="text-center fs-5" style="padding-right:2%"><span class="fw-bold">Price:</span> $${obj.price}<a href="#" class="btn btn-dark px-5" style="margin-left:4%">Buy</a></p>
             </div>
             </div>
-        </div>
-      </div>`;
-}
-
-function htmlDetailsPast(obj) {
-  contenedorDetails.innerHTML = `
-      <div
-        class="card  shadow-lg pb-2 bg-body rounded d-flex flex-column justify-content-center"
-        style="width: 100%; height: 35rem contenedor-cards-general "
-      >
-      <h6 class="text-center pt-1 pb-1 text-bg-dark fs-4 rounded-3">${obj.category}</h6>
-        <div class="row g-0">
-          <div class="col-md-6 p-4">
-            <img
-              src=${obj.image}
-              class="img-fluid rounded-end mt-sm-4 mt-md-0 w-100 h-100"
-              alt="${obj.name}"
-            />
-          </div>
-          <div class="col-md-6">
-            <div class="card-body">
-            
-              <h1 class="card-title text-center fs-3 text-decoration-underline">${obj.name}</h1>
-              <p class="text-center fs-4">${obj.date}</p>
-              <p class="card-text pb-2 fs-5 text-center">
-              ${obj.description}
-              </p>
-              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Place: </span>${obj.place}</p>
-              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Capacity: </span>${obj.capacity}</p>
-              <p class="text-center fs-5" style="padding-right:5%"><span class="fw-bold">Assistance: </span>${obj.assistance}</p>
-              <p class="text-center fs-5" style="padding-right:2%"><span class="fw-bold">Price:</span> $${obj.price}<a href="#" class="btn btn-dark px-5" style="margin-left:4%">Buy</a></p>
-            </div>
-          </div>
         </div>
       </div>`;
 }
@@ -129,8 +93,7 @@ function imprimir(array, ruta) {
 
 function filterFn(fn, value, array, check) {
   applied[fn] = value;
-  console.log(fn);
-  console.log(applied);
+
   check
     ? categoriesFiltradas.push(value)
     : (categoriesFiltradas = categoriesFiltradas.filter(
@@ -146,6 +109,7 @@ function filterFn(fn, value, array, check) {
 
     if (date == "matchWithCheck") {
       let auxiliar = [];
+
       categoriesFiltradas.forEach(
         (categoria) =>
           (auxiliar = auxiliar.concat(
@@ -154,7 +118,6 @@ function filterFn(fn, value, array, check) {
             )
           ))
       );
-      console.log(auxiliar);
       if (auxiliar.length > 0) {
         array = auxiliar;
       }
@@ -164,70 +127,95 @@ function filterFn(fn, value, array, check) {
   return array;
 }
 
-switch (document.title) {
-  case "Upcoming Events-Amazing Events":
-    imprimir(upcoming, hrefOtros);
-    categories.forEach((events) => htmlCheck(events));
-    searchInput.addEventListener("input", (event) => {
-      let escribir = filterFn(
-        "matchWithSearchBar",
-        event.target.value,
-        upcoming
-      );
-      imprimir(escribir, hrefOtros);
-    });
+async function capturar() {
+  try {
+    let api = await fetch("https://mind-hub.up.railway.app/amazing");
+    let data = await api.json();
+    let events = data.events;
+    let currentDate = data.date;
+    let categories = new Set(events.map((element) => element.category).sort());
+    let upcoming = events.filter((event) => event.date >= currentDate);
+    let past = events.filter((event) => event.date < currentDate);
+    switch (document.title) {
+      case "Upcoming Events-Amazing Events":
+        imprimir(upcoming, hrefOtros);
+        categories.forEach((events) => htmlCheck(events));
+        searchInput.addEventListener("input", (event) => {
+          let escribir = filterFn(
+            "matchWithSearchBar",
+            event.target.value,
+            upcoming
+          );
+          imprimir(escribir, hrefOtros);
+        });
 
-    contenedorCheck.addEventListener("change", (event) => {
-      let prueba = filterFn(
-        "matchWithCheck",
-        event.target.value,
-        upcoming,
-        event.target.checked
-      );
-      imprimir(prueba, hrefOtros);
-    });
-    break;
+        contenedorCheck.addEventListener("change", (event) => {
+          let prueba = filterFn(
+            "matchWithCheck",
+            event.target.value,
+            upcoming,
+            event.target.checked
+          );
+          imprimir(prueba, hrefOtros);
+        });
+        break;
 
-  case "Past Events-Amazing Events":
-    imprimir(past, hrefOtros);
-    categories.forEach((events) => htmlCheck(events));
-    searchInput.addEventListener("input", (event) => {
-      let escribir = filterFn("matchWithSearchBar", event.target.value, past);
-      imprimir(escribir, hrefOtros);
-    });
+      case "Past Events-Amazing Events":
+        imprimir(past, hrefOtros);
+        categories.forEach((events) => htmlCheck(events));
+        searchInput.addEventListener("input", (event) => {
+          let escribir = filterFn(
+            "matchWithSearchBar",
+            event.target.value,
+            past
+          );
+          imprimir(escribir, hrefOtros);
+        });
 
-    contenedorCheck.addEventListener("change", (event) => {
-      let prueba = filterFn("matchWithCheck", past, event.target.checked);
-      imprimir(prueba, hrefOtros);
-    });
-    break;
+        contenedorCheck.addEventListener("change", (event) => {
+          let prueba = filterFn("matchWithCheck", past, event.target.checked);
+          imprimir(prueba, hrefOtros);
+        });
+        break;
 
-  case "Details Event Amazing Events":
-    let id = location.search.slice(4);
-    let details = events.filter((element) => element._id === Number(id));
-    details = details[0];
-    details.date < currentDate
-      ? htmlDetailsPast(details)
-      : htmlDetailsUpcoming(details);
+      case "Details Event Amazing Events":
+        let id = location.search.slice(4);
+        let details = events.filter((element) => element.id === id);
+        details = details[0];
+        details.date = details.date.slice(0, 10);
+        console.table(upcoming)
+        details.date < currentDate
+          ? htmlDetails(details,"Assistance","assistance")
+          : htmlDetails(details,"Expectative","estimate");
 
-    break;
+        break;
 
-  default:
-    imprimir(events, hrefHome);
-    categories.forEach((events) => htmlCheck(events));
-    searchInput.addEventListener("input", (event) => {
-      let escribir = filterFn("matchWithSearchBar", event.target.value, events);
-      imprimir(escribir, hrefHome);
-    });
+      default:
+        imprimir(events, hrefHome);
+        categories.forEach((events) => htmlCheck(events));
+        searchInput.addEventListener("input", (event) => {
+          let escribir = filterFn(
+            "matchWithSearchBar",
+            event.target.value,
+            events
+          );
+          imprimir(escribir, hrefHome);
+        });
 
-    contenedorCheck.addEventListener("change", (event) => {
-      let prueba = filterFn(
-        "matchWithCheck",
-        event.target.value,
-        events,
-        event.target.checked
-      );
-      imprimir(prueba, hrefHome);
-    });
-    break;
+        contenedorCheck.addEventListener("change", (event) => {
+          let prueba = filterFn(
+            "matchWithCheck",
+            event.target.value,
+            events,
+            event.target.checked
+          );
+          imprimir(prueba, hrefHome);
+        });
+        break;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+capturar();
+
